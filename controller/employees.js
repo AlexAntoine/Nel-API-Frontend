188826239f31f5e2e462899886ccde047c06bd40
Alex Antoine <antoinealex1234@gmail.com>
2023-04-10 15:52:09 -0400

    added methods for employee

--- controller/employees.js
+++ controller/employees.js
@@ -1,5 +1,25 @@
+const Employee = require('../models/employees');
 
-exports.homePage = (req, res)=>{
+exports.homePage = async(req, res)=>{
+   const employees = await Employee.find();
+    res.render('index',{employees});
+} 
 
-    res.render('index');
-}   
+exports.newEmployee = (req, res)=>{
+    res.render('new');
+}
+
+exports.createEmployee = async(req, res)=>{
+    const {name, designation, salary}= req.body;
+    // console.log(req.body)
+    const newEmployee = new Employee({
+        name,
+        designation,
+        salary
+    });
+
+    const data = await newEmployee.save();
+
+    res.redirect('/');
+    // console.log(data);
+}
