8e9cad67f3699d131affafea290ade5f6d0ab53f
Alex Antoine <antoinealex1234@gmail.com>
2023-04-10 15:51:49 -0400

    added url encoder

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 require('dotenv').config({path:'./.env'});
 const express = require('express');
 const colors = require('colors');
+const { urlencoded } = require('body-parser');
 const {localDb} = require('./db/employeeDb');
 const path = require('path');
 
 const app = express();
 
-//Routes
-const employeeRouter = require('./routes/employee');
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
 
+app.use(urlencoded({extended:true}))
 app.use(express.static('public'));
+
+//Routes
+const employeeRouter = require('./routes/employee');
 
 app.use(employeeRouter);
 
