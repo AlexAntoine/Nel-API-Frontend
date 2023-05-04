f10a2e6f408e46632eb9b9e4ee665458a2cb6195
Alex Antoine <antoinealex1234@gmail.com>
2023-05-04 16:21:54 -0400

    added new controller and route

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
-const {getUsersPage} = require('../controller/users');
+const {getUsersPage, getNewUserPage, getUserEditPage} = require('../controller/users');
 
 router.route('/users').get(getUsersPage);
+router.route('/edit/users/:id').get(getUserEditPage);
+router.route('/adduser').get(getNewUserPage);
 
 module.exports = router;
