9837fcfc9c7a1aef7999232dd19425943b718be9
Alex Antoine <antoinealex1234@gmail.com>
2023-05-04 16:21:39 -0400

    remove route

--- routes/employee.js
+++ routes/employee.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
-const {homePage, getTestPage, updateEmployee, deleteEmployee,getEditPage, newEmployee,createEmployee,findOneEmployee, searchEmployee,editEmployee } = require('../controller/employees');
+const {homePage, getTestPage, updateEmployee, deleteEmployee,newEmployee,createEmployee,findOneEmployee, searchEmployee,editEmployee } = require('../controller/employees');
 
 router.route('/').get(homePage);
 router.route('/search').get(searchEmployee);
 router.route('/edit/:id').get(editEmployee);
 router.route('/edit/:id').put(updateEmployee);
 router.route('/delete/:id').delete(deleteEmployee);
-router.route('/edit').get(getEditPage)
+
 router.route('/employee').get(findOneEmployee);
 router.route('/new').get(newEmployee).post(createEmployee);
 router.route('/test').get(getTestPage);
