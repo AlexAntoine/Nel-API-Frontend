9408222641d023c9db581ce9186210049fe3e6b5
Alex Antoine <antoinealex1234@gmail.com>
2023-05-04 16:21:28 -0400

    added new user page and edit user page

--- controller/users.js
+++ controller/users.js
@@ -1,9 +1,19 @@
 const axios = require('axios');
 
-exports.getUsersPage = async(req, res)=>{
+exports.getNewUserPage = async(req, res)=>{
 
-    const {data} = await axios.get('https://nel-api.herokuapp.com/api/nelusers');
+    res.render('addUser');
+}
+
+exports.getUserEditPage = async(req, res)=>{
+    console.log(req.params);
+
+    res.render('editUser', {users:''})
+}
+
+exports.getUsersPage = async(req, res)=>{
     
+    const {data} = await axios.get('https://nel-api.herokuapp.com/api/nelusers');
     console.log(data.data);
-    res.render('users',{users:data.data});
+    res.render('users', {users:data.data});
 }
