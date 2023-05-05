9154a5d4bdef2f25a40b9065e9b43f10f6c57bae
Alex Antoine <antoinealex1234@gmail.com>
2023-05-05 13:23:49 -0400

    added deviceage controller method

--- controller/deviceAge.js
+++ controller/deviceAge.js
@@ -7,3 +7,10 @@ exports.getDeviceAgePage = async(req, res)=>{
     res.render('deviceage', {devices:devices.data.data});
 
 }
+
+exports.getDeviceAgeEditPage = async(req,res)=>{
+
+    const {data} = await axios.get(`https://nel-api.herokuapp.com/api/deviceage/${req.params.id}`);
+
+    res.render('editDeviceAge',{devices:data.data})
+}
