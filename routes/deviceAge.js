a42cdd496ee659f1f7f9aafb6e6d98f1b58ab90e
Alex Antoine <antoinealex1234@gmail.com>
2023-05-05 13:24:09 -0400

    added route and controller method

--- routes/deviceAge.js
+++ routes/deviceAge.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {getDeviceAgePage} = require('../controller/deviceAge');
+const {getDeviceAgePage, getDeviceAgeEditPage} = require('../controller/deviceAge');
 
 router.route('/deviceage').get(getDeviceAgePage);
+router.route('/edit/deviceage/:id').get(getDeviceAgeEditPage);
 
 module.exports = router;
