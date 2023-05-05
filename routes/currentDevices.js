fceacf8d2ae7f9d4597326c36dfa607b8b7cd7b5
Alex Antoine <antoinealex1234@gmail.com>
2023-05-05 15:51:06 -0400

    added router and method to get current page method

--- routes/currentDevices.js
+++ routes/currentDevices.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {getCurrentDevicePage} = require('../controller/currentDevices');
+const {getCurrentDevicePage,getCurrentDeviceEditPage} = require('../controller/currentDevices');
 
 const router = express.Router();
 
 router.route('/current').get(getCurrentDevicePage);
+router.route('/edit/current/:id').get(getCurrentDeviceEditPage);
 
 module.exports = router;
