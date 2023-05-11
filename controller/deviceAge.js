c3d414fc50bab8aafe8885be25da04ec3e98b638
Alex Antoine <antoinealex1234@gmail.com>
2023-05-11 15:47:14 -0400

    create function to update device

--- controller/deviceAge.js
+++ controller/deviceAge.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 exports.getDeviceAgePage = async(req, res)=>{
     
     const devices = await axios.get('https://nel-api.herokuapp.com/api/deviceage');
-
+    
     res.render('deviceage', {devices:devices.data.data});
 
 }
@@ -13,4 +13,17 @@ exports.getDeviceAgeEditPage = async(req,res)=>{
     const {data} = await axios.get(`https://nel-api.herokuapp.com/api/deviceage/${req.params.id}`);
 
     res.render('editDeviceAge',{devices:data.data})
+}
+
+exports.updateDeviceAge = async(req, res)=>{
+    const {devicename,shipdate} = req.body;
+
+    const data =  {
+        DeviceName:devicename,
+        ShipDate:shipdate
+
+    }
+    await axios.put(`https://nel-api.herokuapp.com/api/deviceage/${req.params.id}`, data);
+    
+    res.redirect('/deviceage')
 }
