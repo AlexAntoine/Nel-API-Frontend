d1d6ce3d152ba005ba6df28094c873d9ee6382b1
Alex Antoine <antoinealex1234@gmail.com>
2023-05-11 15:47:24 -0400

    added router for update

--- routes/deviceAge.js
+++ routes/deviceAge.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {getDeviceAgePage, getDeviceAgeEditPage} = require('../controller/deviceAge');
+const {getDeviceAgePage, getDeviceAgeEditPage,updateDeviceAge} = require('../controller/deviceAge');
 
 router.route('/deviceage').get(getDeviceAgePage);
-router.route('/edit/deviceage/:id').get(getDeviceAgeEditPage);
+router.route('/edit/deviceage/:id').get(getDeviceAgeEditPage).put(updateDeviceAge);
 
 module.exports = router;
