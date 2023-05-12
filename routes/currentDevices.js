d3dde80bf819fe6c861ec20bcf9c1045358abdd6
Alex Antoine <antoinealex1234@gmail.com>
2023-05-12 14:46:51 -0400

    add route to access add current page

--- routes/currentDevices.js
+++ routes/currentDevices.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {getCurrentDevicePage,getCurrentDeviceEditPage} = require('../controller/currentDevices');
+const {getCurrentDevicePage,getCurrentDeviceEditPage, getAddCurrentDevicePage} = require('../controller/currentDevices');
 
 const router = express.Router();
 
 router.route('/current').get(getCurrentDevicePage);
 router.route('/edit/current/:id').get(getCurrentDeviceEditPage);
+router.route('/add/current').get(getAddCurrentDevicePage)
 
 module.exports = router;
