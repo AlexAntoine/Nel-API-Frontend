bdad6e6e3ca4f72c5035dc3aa1f28890b0bec977
Alex Antoine <antoinealex1234@gmail.com>
2023-05-26 15:29:46 -0400

    add logout function

--- controller/login.js
+++ controller/login.js
@@ -1,6 +1,20 @@
+const passport = require('passport')
 exports.getLoginPage = async(req, res, next)=>{
   
     res.render('login');
+
+}
+
+exports.logOut = async(req, res, next)=>{
+
+    req.logout((err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash('success_msg', 'You have been logged out');
+        res.redirect('/')
+    });
+   
 
 }
 
