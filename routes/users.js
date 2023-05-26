d60c7730dc2ab453473adc546ed2022a04e4b88e
Alex Antoine <antoinealex1234@gmail.com>
2023-05-26 15:30:25 -0400

    brought in auth middleware

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const {isAuthenticated} = require('../middleware/auth');
 const router = express.Router();
 const {getUsersPage, getNewUserPage, getUserEditPage, updateUser, addNewUser, deleteUser} = require('../controller/users');
 
-router.route('/users').get(getUsersPage);
+router.route('/users').get(isAuthenticated, getUsersPage);
 router.route('/edit/users/:id').get(getUserEditPage).put(updateUser);
 router.route('/add/users').get(getNewUserPage).post(addNewUser);
 router.route('/delete/:id').post(deleteUser);
