1e091dad29adfe670c957bc25b2600a800d6a1c7
Alex Antoine <antoinealex1234@gmail.com>
2023-06-22 19:54:36 -0400

    configure new login procedure

--- controller/login.js
+++ controller/login.js
@@ -1,8 +1,27 @@
-const passport = require('passport')
+const User = require('../models/users');
+const {loginApi} = require('../utils/userCalls');
+
 exports.getLoginPage = async(req, res, next)=>{
   
     res.render('login');
 
+}
+
+exports.userLogin = async(req, res, next)=>{
+    const {email, password} = req.body;
+    const user = await User.findByCredentials(email, password);
+    // console.log(user);
+    // console.log(req);
+    // console.log('user: ',user);
+    const token = await loginApi(req);
+    console.log('token: ',token);
+
+    user.tokens = user.tokens.concat({token});
+    res.cookie('token',token)
+
+    const result = await user.save();
+    console.log('result: ',result);
+    res.redirect('/users');
 }
 
 exports.logOut = async(req, res, next)=>{
