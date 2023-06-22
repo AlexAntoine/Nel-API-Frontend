dc6ac3b4a94de0e61c3fc7d9b0571edd4be14f6b
Alex Antoine <antoinealex1234@gmail.com>
2023-06-22 19:56:46 -0400

    brought in new users method

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const {auth} = require('../middleware/auth');
 const router = express.Router();
-const {getUsersPage, getNewUserPage, getUserEditPage, updateUser, addNewUser, deleteUser, getHome, apiLogin, } = require('../controller/users');
+const {getUsersPage, getNewUserPage, getUserEditPage, updateUser, addNewUser, deleteUser} = require('../controller/users');
 
 router.route('/users').get(getUsersPage);
-// router.route('/home').get(isAuthenticated, getHome).post(apiLogin);
 router.route('/edit/users/:id').get(getUserEditPage).put(updateUser);
 router.route('/add/users').get(auth,getNewUserPage).post(addNewUser);
 router.route('/delete/:id').post(deleteUser);
