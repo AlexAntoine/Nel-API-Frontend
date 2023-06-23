038e1e1dd84876691f501179b30b5da742cdc7d0
Alex Antoine <antoinealex1234@gmail.com>
2023-06-23 12:36:28 -0400

    comment out lines

--- controller/signup.js
+++ controller/signup.js
@@ -11,12 +11,12 @@ exports.sendNewUser = async(req, res)=>{
     const newUser = await User.create(req.body);
 
     const token = await loginApi(req);
-    console.log('line 13 Token: ', token);
+    // console.log('line 13 Token: ', token);
 
     newUser.tokens = newUser.tokens.concat({token})
 
     const result = await newUser.save();
-    console.log('line 19 result: ',result);
+    // console.log('line 19 result: ',result);
 
     res.cookie('token',token,{
         httpOnly:true
