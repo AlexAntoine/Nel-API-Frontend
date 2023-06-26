5127fbf3641c1052bbd6c8e2bfe4be96c584e3f7
Alex Antoine <antoinealex1234@gmail.com>
2023-06-26 16:42:41 -0400

    added try catch to auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,19 +3,28 @@ const User = require('../models/users');
 
 exports.auth = async(req, res, next)=>{
   
-    token = req.cookies.token;
-    // console.log('line 7: ', token);
-
-    const decoded = jwt.verify(token,'abcd');
-    // console.log('decoded: ', decoded);
-
-    const user = await User.findOne({email:decoded.email, 'tokens.token':token});
-    // console.log('auth 13: ',user);
-    if(!user){
-        throw new Error('Something went wrong')
-    }
+    try{
+        token = req.cookies.token;
+        // console.log('line 7: ', token);
     
-    req.token = token;
-    req.user = user;
-    next();
+        const decoded = jwt.verify(token,'abcd');
+        // console.log('decoded: ', decoded);
+    
+        const user = await User.findOne({email:decoded.email, 'tokens.token':token});
+        // console.log('auth 13: ',user);
+        if(!user){
+            throw new Error('Something went wrong')
+        }
+        
+        req.token = token;
+        req.user = user;
+        next();
+        
+    }catch(error){
+        console.log('Error: ',error);
+        req.flash('error_msg', 'Please log In!')
+        res.redirect('/')
+    }
 }
+    
+   
