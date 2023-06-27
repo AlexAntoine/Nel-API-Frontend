47f43adf2b1325b2d4aabddad1f9bd9b3710b1f9
Alex Antoine <antoinealex1234@gmail.com>
2023-06-27 16:36:50 -0400

    added auth middleware

--- routes/deviceAge.js
+++ routes/deviceAge.js
@@ -4,8 +4,8 @@ const {auth} = require('../middleware/auth');
 const {getDeviceAgePage,addDevicePage, getDeviceAgeEditPage,updateDeviceAge, addNewDeviceAge, deleteDevice} = require('../controller/deviceAge');
 
 router.route('/deviceage').get(auth, getDeviceAgePage);
-router.route('/edit/deviceage/:id').get(getDeviceAgeEditPage).put(updateDeviceAge);
-router.route('/add/deviceage').get(addDevicePage).post(addNewDeviceAge);
+router.route('/edit/deviceage/:id').get(auth,getDeviceAgeEditPage).put(updateDeviceAge);
+router.route('/add/deviceage').get(auth,addDevicePage).post(addNewDeviceAge);
 router.route('/delete/:id').post(deleteDevice);
 
 module.exports = router;
