9c945c0ade8a0c37bba6af941a7303694e9601a1
Alex Antoine <antoinealex1234@gmail.com>
2023-06-28 16:12:27 -0400

    add  method

--- routes/deviceAge.js
+++ routes/deviceAge.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {auth} = require('../middleware/auth');
-const {getDeviceAgePage,addDevicePage, getDeviceAgeEditPage,updateDeviceAge, addNewDeviceAge, deleteDevice} = require('../controller/deviceAge');
+const {getDeviceAgePage,getAddDevicePage,getDeviceAgeEditPage,updateDeviceAge, addNewDeviceAge, deleteDevice} = require('../controller/deviceAge');
 
 router.route('/deviceage').get(auth, getDeviceAgePage);
 router.route('/edit/deviceage/:id').get(auth,getDeviceAgeEditPage).put(updateDeviceAge);
-router.route('/add/deviceage').get(auth,addDevicePage).post(addNewDeviceAge);
+router.route('/add/deviceage').get(auth,getAddDevicePage).post(addNewDeviceAge);
 router.route('/delete/:id').post(deleteDevice);
 
 module.exports = router;
