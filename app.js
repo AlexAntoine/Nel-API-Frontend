ef62cfd0e1a2e0e01497518e917e62e5f018d640
Alex Antoine <antoinealex1234@gmail.com>
2023-06-30 15:39:01 -0400

    again

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const path = require('path');
 
 const app = express();
 console.log(__dirname +'/views');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine','ejs');
 
 app.use(urlencoded({extended:true}))
