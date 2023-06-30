a47ac45fe7d193abe2005919759d1be8668c657c
Alex Antoine <antoinealex1234@gmail.com>
2023-06-30 15:00:48 -0400

    again

--- controller/currentDevices.js
+++ controller/currentDevices.js
@@ -3,17 +3,18 @@ const {getCurrentDevices, addNewDevice, updateCurrentDevice, getSingleCurrentDev
 
 // Get
 exports.getCurrentDevicePage = async(req, res)=>{
-
+    console.log('hello');
     try{
         const {token} = req.cookies;
-
+        console.log(token);
         const data = await getCurrentDevices(token);
+        console.log(data);
 
         if(!data){
             throw new Error('No data to display')
         }
     
-        res.render('currentDevices',{devices:[]});
+        res.render('currentDevices',{devices:data});
 
     }catch(error){
         console.log(error);
