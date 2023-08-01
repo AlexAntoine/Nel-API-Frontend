88e44f043e5c1bb82f3a761cf6f34f9698558ac9
Alex Antoine <antoinealex1234@gmail.com>
2023-08-01 15:40:49 -0400

    added routes to download feature

--- routes/currentDevices.js
+++ routes/currentDevices.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {auth} = require('../middleware/auth');
-const {getCurrentDevicePage,getCurrentDeviceEditPage,getAddCurrentDevicePage,updateCurrentDevice, addCurrentDevice, deleteCurrentDevice} = require('../controller/currentDevices');
+const {getCurrentDevicePage,getCurrentDeviceEditPage,getAddCurrentDevicePage,updateCurrentDevice, addCurrentDevice, deleteCurrentDevice, downloadCurrentDevices} = require('../controller/currentDevices');
 
 const router = express.Router();
 
@@ -8,5 +8,5 @@ router.route('/current').get(auth, getCurrentDevicePage);
 router.route('/edit/current/:id').get(auth, getCurrentDeviceEditPage).put(updateCurrentDevice);
 router.route('/add/current').get(auth, getAddCurrentDevicePage).post(addCurrentDevice);
 router.route('/current/delete/:id').post(auth, deleteCurrentDevice);
-
+router.route('/current/download').get(downloadCurrentDevices);
 module.exports = router;
