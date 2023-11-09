6431369086779a16648817f89d91f1a6ef2a77a1
Alex Antoine <antoinealex1234@gmail.com>
2023-11-09 11:25:30 -0500

    Delete CR API method call

--- utils/currentRecordsCalls.js
+++ utils/currentRecordsCalls.js
@@ -11,3 +11,15 @@ exports.getAllCurrentRecords = async(token)=>{
 
     return data.data;
 }
+
+// Delete Current Records
+exports.deleteCurrentRecordCall = async(id, token)=>{
+
+   const {data} =await axios.delete(`https://nel-api.herokuapp.com/api/records/${id}`, {
+        headers:{
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    
+    return data;
+}
